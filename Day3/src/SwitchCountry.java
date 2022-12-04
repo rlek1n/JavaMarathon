49d7bf487599239036dfca716bf3cf4df74802b0
rlek1n <manhax94@gmail.com>
2022-12-04 12:41:58 +0300

    init

---
+++ Day3/src/SwitchCountry.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+/**
+ * Реализовать программу, которая в консоль выводит название страны, принимая на
+ * вход название города. Программа должна работать до тех пор, пока не будет введено
+ * слово “Stop”.
+ * Реализовать, используя следующие данные:
+ * Москва, Владивосток, Ростов - Россия
+ * Рим, Милан, Турин - Италия
+ * Ливерпуль, Манчестер, Лондон - Англия
+ * Берлин, Мюнхен, Кёльн - Германия
+ * При вводе любого другого города, вывести сообщение “Неизвестная страна”.
+ */
+public class SwitchCountry {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNext("Stop")) {
+            String city = scanner.nextLine();
+            switch (city) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                case "Кельн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+}
